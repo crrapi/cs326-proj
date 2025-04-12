4c6b43dc8799cb34d69f38f3dd4d39137f247f78
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2025-04-12 01:02:44 -0400

    Making Input Work

--- client/JSFrontendComponents/portfolioInput.js
+++ client/JSFrontendComponents/portfolioInput.js
@@ -1,25 +1,42 @@
-let portfolioInput = document.getElementsByClassName("portfolio-input")[0]
+let portfolioInput = document.getElementsByClassName("portfolio-input")[0];
 portfolioInput.innerHTML = `
-    <h2>Add Stock</h2>
-    <form>
+    <h2>Buy/Sell Stock</h2>
+    <form class="buy-sell-stock-form">
         <div class="form-group">
             <label for="ticker">Ticker Symbol</label>
             <input type="text" id="ticker" placeholder="e.g., AAPL">
-            <!-- TODO autocomplete / debounced lookup -->
-        </div>
-        <div class="form-group">
-            <label for="quantity">Quantity</label>
-            <input type="number" id="quantity" placeholder="Number of shares">
         </div>
         <div class="form-group">
             <label for="purchase-date">Purchase Date</label>
             <input type="date" id="purchase-date">
         </div>
         <div class="form-group">
-            <label for="purchase-price">Purchase Price</label>
-            <input type="number" id="purchase-price" placeholder="Price per share">
+            <label for="quantity">Quantity</label>
+            <input type="number" id="quantity" placeholder="Number of shares">
+        </div>
+        <div class="form-group">
+            <label for="total-price">Purchase Price</label>
+            <input type="number" id="total-price" placeholder="Total Price">
         </div>
         <button type="submit" class="btn">Add to Portfolio</button>
-        <!-- TODO onSubmit add to local storage, or hit backend API -->
     </form>
-`
+`;
+
+let buySellStockForm = document.getElementsByClassName("buy-sell-stock-form")[0];
+buySellStockForm.addEventListener('submit', (e) => {
+    e.preventDefault(); // Prevent Page Reload
+
+    let ticker = document.getElementById("ticker").value
+    let purchaseDate = document.getElementById("purchase-date").value
+    let quantity = document.getElementById("quantity").value
+    let totalPrice = document.getElementById("total-price").value
+
+    
+
+    
+
+    console.log({"ticker": ticker, "purchaseDate": purchaseDate, "quantity": quantity, "totalPrice": totalPrice})
+
+
+    console.log(e);     // This should now work
+});
