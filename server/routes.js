c13721952dfae2ac7e4ae129996307ceca532251
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2025-04-25 22:46:57 -0400

    forgot to git add . when I removed duplicate file from serverside code

--- server/routes.js
+++ server/routes.js
@@ -4,7 +4,6 @@ const controllers = require('./controllers');
 const router = express.Router();
 
 router.get('/portfolio', controllers.getPortfolio);
-
 router.get('/portfolio/realtime-graph', controllers.getRealtimeGraphData);
 
 module.exports = router;
