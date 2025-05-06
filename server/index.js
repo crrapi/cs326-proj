92a48093134ddc50637b5abcb9e705a25a60c0e9
Christopher R <39552369+crrapi@users.noreply.github.com>
2025-05-06 17:08:10 -0400

    [feature] Implement full database usage, remove temp values, RESTORE buy/sell buttons

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const app = require('./app');
+const { sequelize } = require('./models');
 
 const PORT = process.env.PORT || 3000;
+
+sequelize.sync().then(() => console.log('Database synced'));
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
--- server/routes.js
+++ server/routes.js
@@ -8,4 +8,16 @@ router.post('/portfolio/buy', controllers.buyStock);
 router.post('/portfolio/sell', controllers.sellStock);
 router.get('/portfolio/realtime-graph', controllers.getRealtimeGraphData);
 
+router.get('/users', controllers.listUsers);
+router.post('/users', controllers.createUser);
+router.get('/users/:userId', controllers.getUser);
+router.put('/users/:userId', controllers.updateUser);
+router.delete('/users/:userId', controllers.deleteUser);
+
+router.get('/holdings', controllers.listHoldings);
+router.post('/holdings', controllers.createHolding);
+router.get('/holdings/:id', controllers.getHolding);
+router.put('/holdings/:id', controllers.updateHolding);
+router.delete('/holdings/:id', controllers.deleteHolding);
+
 module.exports = router;
